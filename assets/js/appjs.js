37b0587f3c375565cfefb5d9ba5738ec7f9d653a
Cohen <dcoco91@gmail.com>
2019-06-28 15:55:15 -0400

    created background, table on page, form for inputting train, and a ton of style options were created

--- assets/js/appjs.js
+++ assets/js/appjs.js
@@ -1,5 +1,13 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAdP3fqvk2gWk2htYIN9yQL13iYQWriReQ",
+//     authDomain: "traintime-62eea.firebaseapp.com",
+//     databaseURL: "https://traintime-62eea.firebaseio.com",
+//     projectId: "traintime-62eea",
+//     storageBucket: "traintime-62eea.appspot.com",
+//     messagingSenderId: "279212392121",
+//     appId: "1:279212392121:web:69c43097f551c2e2"
+//   }
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  var database = firebase.database();
+//   var database = firebase.database();
